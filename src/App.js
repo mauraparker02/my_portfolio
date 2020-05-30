3e3065ea126d8cd61da8eda7ce61b2bb7215aacf
mauraparker02 <mauraparker02@gmail.com>
2020-05-30 18:49:03 -0400

    adding a readme

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="wrapper">
       <Navbar></Navbar>
-      <img src="./public/imgs/me.jpg" alt="not working" width="500" height="600"></img>
+      <img src="me.jpg" alt="not working" width="500" height="600">
+      </img>
+
       <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
       <h1>Portfolio</h1>
       <PortCard></PortCard>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -19,37 +19,17 @@ function Navbar(props) {
     }}
     trigger={<Button node="button">=</Button>}
   >
-    {/* <SideNavItem
-      user={{
-        background: 'https://placeimg.com/640/480/tech',
-        email: 'jdandturk@gmail.com',
-        image: 'static/media/react-materialize-logo.824c6ea3.svg',
-        name: 'John Doe'
-      }}
-      userView
-    /> */}
-    <SideNavItem
-      href="../public/logo.png"
-      icon={<Icon>cloud</Icon>}
-    ></SideNavItem>
-    {/* <SideNavItem href="#!second">
-      Second Link
-    </SideNavItem> */}
-    <SideNavItem divider />
-    {/* <SideNavItem subheader>
-      Subheader
-    </SideNavItem> */}
     <SideNavItem
       href="#!portfolio"
       waves
     >
-      Portfolio 
+      About  
     </SideNavItem>
     <SideNavItem
       href="#!about"
       waves
     >
-      About
+      Portfolio
     </SideNavItem>
     <SideNavItem
       href="#!about"
